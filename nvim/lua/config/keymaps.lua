e261e82c20bd350cc872e199f9f2e5b4cf47f04f
Jason <jason.ang@collabocode.com>
2026-07-14 17:59:34 +0700

    (neovim) add more yanking utilities

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -27,3 +27,89 @@ vim.keymap.set("x", "gy", function()
   vim.fn.setreg("+", ref)
   vim.notify("Copied: " .. ref)
 end, { desc = "Copy filepath + line(s) of selection" })
+
+-- Case conversion utilities
+local function split_words(s)
+  s = s:gsub("([a-z0-9])([A-Z])", "%1 %2") -- camelCase boundary
+  s = s:gsub("([A-Z]+)([A-Z][a-z])", "%1 %2") -- HTTPServer -> HTTP Server
+  s = s:gsub("[_%-%s]+", " ")
+  local words = {}
+  for w in s:gmatch("%S+") do
+    table.insert(words, w:lower())
+  end
+  return words
+end
+
+local converters = {
+  kebab = function(w)
+    return table.concat(w, "-")
+  end,
+  snake = function(w)
+    return table.concat(w, "_")
+  end,
+  camel = function(w)
+    local out = w[1]
+    for i = 2, #w do
+      out = out .. w[i]:gsub("^%l", string.upper)
+    end
+    return out
+  end,
+  pascal = function(w)
+    local out = ""
+    for _, x in ipairs(w) do
+      out = out .. x:gsub("^%l", string.upper)
+    end
+    return out
+  end,
+  screaming = function(w)
+    return table.concat(w, "_"):upper()
+  end,
+  title = function(w)
+    local parts = {}
+    for _, x in ipairs(w) do
+      table.insert(parts, (x:gsub("^%l", string.upper)))
+    end
+    return table.concat(parts, " ")
+  end,
+}
+
+-- Get visually selected text (or current WORD in normal mode)
+local function get_text(mode)
+  if mode == "v" then
+    local save = vim.fn.getreg("z")
+    vim.cmd('noautocmd normal! "zy')
+    local txt = vim.fn.getreg("z")
+    vim.fn.setreg("z", save)
+    return txt
+  else
+    return vim.fn.expand("<cword>")
+  end
+end
+
+local function yank_as(kind, mode)
+  local txt = get_text(mode)
+  local result = converters[kind](split_words(txt))
+  vim.fn.setreg("+", result)
+  vim.fn.setreg('"', result)
+  vim.notify("Yanked: " .. result)
+end
+
+-- Keymaps: <leader>y + case letter
+local map = vim.keymap.set
+local kinds = {
+  k = "kebab",
+  s = "snake",
+  c = "camel",
+  p = "pascal",
+  u = "screaming",
+  t = "title",
+}
+
+for key, kind in pairs(kinds) do
+  map("n", "<leader>y" .. key, function()
+    yank_as(kind, "n")
+  end, { desc = "Yank word as " .. kind .. "-case" })
+  map("x", "<leader>y" .. key, function()
+    yank_as(kind, "v")
+  end, { desc = "Yank selection as " .. kind .. "-case" })
+end
