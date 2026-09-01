35e954298369327e8cbe118d844f8058950263ff
Jason <jason.ang@collabocode.com>
2026-09-01 13:32:05 +0700

    (neovim) disable smart find bind in flash.nvim

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    modes = {
+      char = {
+        char_actions = function()
+          return {
+            [";"] = "next",
+            [","] = "prev",
+          }
+        end,
+      },
+    },
+  },
+}
